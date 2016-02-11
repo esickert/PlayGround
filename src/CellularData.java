69559b51d96612e3e85a494684d458a825c32a51
esickert <esickert@gmail.com>
2016-02-10 17:43:13 -0800

    added table printout

--- src/CellularData.java
+++ src/CellularData.java
@@ -1,28 +1,51 @@
 public class CellularData	{
 
-	int beginYear = 1980;
-	int endYear = 1987;
-    int num = 1880;
-    
-    Object[] header = new Object[8]; 		//Single array for header.
-	Object[][] anArray = new Object[5][8];  //this is an array of Object class
+	private int startingYear;
+	private int rows = 4;
+	private int columns = 8;
 	
-	CellularData()	{
+	public CellularData()	{
 	}
+	
+	public CellularData(int numOfRows, int numOfColumns, int startingYear)	{
+		this.rows = numOfRows;
+		this.columns = numOfColumns;
+		this.startingYear = startingYear;
+	}
+
+	Object[] header = new Object[columns]; 		//Single array for header.
+	Object[][] anArray = new Object[rows][columns];  //this is an array of Object class
 
 	/**
 	 *  creates a header of 1D array for the 2D array with country and years	
 	 *  **/
-	 
+	 //*************************************************************************
 	void printHeader(CellularData x)	{
 		System.out.print(x.header[0] = "Country" + "\t"); 
 		for(int i = 1; i <= x.header.length - 1; i++)	{
-			System.out.print(x.header[i] = x.beginYear++ + "\t");
+			System.out.print(x.header[i] = x.startingYear++ + "\t");
 		}
 		System.out.println();
 	} //end of method printHeader
 	
+	//*************************************************************************
+	public Object[][] addCountry(double[] a, String country, int rowNumber)	{ //method needs closing bracket
+			anArray[rowNumber][0] = country;
+			for (int i = 0; i <= a.length - 1; i++)	{
+				anArray[rowNumber][i + 1] = a[i];
+			}
+			return anArray;
+		} 
+//*****************************************************************************	
+	public double getNumSubscriptionsInCountryForPeriod(double a[])	{
+		double temp = 0.0;
+		for (int i = 1; i <= a.length -1; i++)	{
+			temp = temp + a[i];
+		}
+		 return temp;
+	}
 
+//*****************************************************************************
 	public String toString()	{ //this works
 		String output = "";
 		for(int i = 0; i <= anArray.length - 1; i++)	{
--- src/TestCircle.java
+++ src/TestCircle.java
@@ -5,21 +5,31 @@ public class TestCircle   {
 	static int num = 2080;
 
 	public static void main(String[] args)  {
+	
+		System.out.println("Part 1 with Partial Data:");
+		final double[] canadaPartial = {0,0,0.05,0.23,0.37,0.75,1.26};
+		final double[] mexicoPartial = {0,0,0,0,0,0,0.01};
+		final double[] usaPartial = {0,0,0.14,0.28,0.5,0.83,1.39};
+		final double[] albaniaPartial = {0,0.9014,60.067,73.3,58.912,78.18,85.4682};
+	
+		int startingYear = 1983;
+		CellularData table;
+		int numRows = 4;
+		int numColumns = albaniaPartial.length;
 
-		CellularData table = new CellularData(); //one instance(object) represents one table
+		System.out.println(numRows + "  " +numColumns);
+		table = new CellularData(4, numColumns, startingYear); //parameters still not working*****************************
 //		TestCircle y = new TestCircle(); //this is an instance of TestCircle
 
-		System.out.println("Part 1 with Partial Data:");
-//		final double[] canadaPartial = {0,0,0.05,0.23,0.37,0.75,1.26};
-//		final double[] mexicoPartial = {0,0,0,0,0,0,0.01};
-//		final double[] usaPartial = {0,0,0.14,0.28,0.5,0.83,1.39};
-		final double[] albaniaPartial = {0,0.9014,60.067,73.3,58.912,78.18,85.4682};
+		
+
+		
 
 //		System.out.println(x.getRadius() + " getRadius is in the Circle class\n");
 //		System.out.println(x.getLength());
 //		System.out.println(x.findArea());
-		tester(111, table);
-		System.out.println();
+//		tester(111, table);
+//		System.out.println();
 		
 //		System.out.println(table);
 		
@@ -28,38 +38,34 @@ public class TestCircle   {
 // BETWEEN STAR LINES IS TEST CODE FOR BITA 1
 //	test[rows][columns]
 
-// NOTE: we are using an array of Object (THE OBJECT CLASS) 
 
-//		System.out.println(x.anArray[0][0]);
-		table.anArray[1][1] = 0.123;
-		table.anArray[2][4] = 0.043;
+//		System.out.println();
+//		System.out.println(table);  //this prints out an empty table of "null" values
+	
+		table.addCountry(mexicoPartial, "mexico", 0);
+		table.addCountry(usaPartial, "usa",1);
+		table.addCountry(canadaPartial, "canada", 2);
+		table.addCountry(albaniaPartial, "albania", 3);
+
+//		for(int i = 0; i <= table.anArray.length - 1; i++)	{//COLUMNS Cylinder uses the array defined in Circle
+//			for(int j = 0; j <= table.anArray[i].length - 1; j++)	{  //ROWS
+//				System.out.print(table.anArray[i][j] + "\t");
+//			}
+//			System.out.println();
+//		}
 
 		System.out.println();
-//		System.out.println(table);
-		
-//		public void insertArray(double[][] a, String country)	{ //method needs closing bracket
-			table.anArray[3][0] = "albania";
-			for (int i = 0; i <= albaniaPartial.length - 1; i++)	{
-				table.anArray[3][i + 1] = albaniaPartial[i];
-			}
-		table.printHeader(table);
-
-		for(int i = 0; i <= table.anArray.length - 1; i++)	{//COLUMNS Cylinder uses the array defined in Circle
-			for(int j = 0; j <= table.anArray[i].length - 1; j++)	{  //ROWS
-				System.out.print(table.anArray[i][j] + "\t");
-			}
-			System.out.println();
-		}
-
-		System.out.println("\n");
 		table.printHeader(table);
 		System.out.println(table);
+		System.out.printf("usa (1983 to 1989): %.2f \n", table.getNumSubscriptionsInCountryForPeriod(usaPartial));
+//		table.getNumSubscriptionsInCountryForPeriod(usaPartial);
+		
 	}//end of main
 	
 //****************************************************************************** 
 	
 /**	KEYWORD "this" TESTING.    **/ 	
-	static void tester(int num, CellularData z)	{
+/*	static void tester(int num, CellularData z)	{
 	//no object call with a static method thus no class object(instance). 'this' doesn't work
 		System.out.println("Trying to understand the 'this' keyword!!!");
 		System.out.println(num + " :method parameter variable");
@@ -71,5 +77,5 @@ public class TestCircle   {
 //		z.num = this.num;
 //		'z' is an instance (object) thus cannot be used in a static method
 		System.out.println(num + " :line 67 setting Circle object variable to local instance variable");
-	}//end of method tester
+	}//end of method tester  */
 } //end of class TestCircle
